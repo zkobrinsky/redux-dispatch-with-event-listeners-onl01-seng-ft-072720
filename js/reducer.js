f6d548d51804b78d46e236aac3177cbd9c24c463
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-12-07 14:40:00 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,28 @@
-// add code snippets from README
+let state = {count: 0}
+ 
+function reducer(state, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
+
+function dispatch(action){
+    state = reducer(state, action);
+    render();
+    return state;
+  }
+
+  function render() {
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+  }
+
+  function mountButton() {
+    document.querySelector("#button").addEventListener('click', () => dispatch({type: 'INCREASE_COUNT'}))
+  }
+
+  dispatch({type: 'fuck'})
+  mountButton()
